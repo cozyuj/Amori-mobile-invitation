88a31c2471723fb25486c58a37348af0155baa75
Jin Yu <yjin887@gmail.com>
2025-11-05 14:03:39 +0900

    chore: navigate삭제

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect} from 'react';
 import { login, getGoogleLoginUrl, getNaverLoginUrl } from '../services/api';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Button, Input, Form, Spin } from 'antd';
 import './MainPage.css';
 
